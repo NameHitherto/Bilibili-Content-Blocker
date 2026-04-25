ad5540285e6b5e7446b2c84df6e5f9ada51b1b88
NameHitherto <605351778@qq.com>
2026-04-26 02:23:46 +0800

    feat: add background service worker

---
+++ background/background.js
@@ -0,0 +1,17 @@
+chrome.runtime.onInstalled.addListener(async () => {
+  const enabled = await chrome.storage.local.get("enabled");
+  if (enabled.enabled === undefined) {
+    await chrome.storage.local.set({ enabled: false });
+  }
+
+  const blockMode = await chrome.storage.local.get("blockMode");
+  if (blockMode.blockMode === undefined) {
+    await chrome.storage.local.set({ blockMode: "blackout" });
+  }
+});
+
+chrome.action.onClicked.addListener((tab) => {
+  if (tab.id) {
+    chrome.tabs.sendMessage(tab.id, { action: "toggle" });
+  }
+});
